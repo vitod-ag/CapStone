7878d4e11ba556a1cded4651f7110990c3180cf1
vitod-ag <116908757+vitod-ag@users.noreply.github.com>
2024-06-15 11:37:07 +0200

    commit sistemazione orizzontale del grid system e correzione del ts

--- FrontEnd/WinningStrategy/src/app/components/pitch/pitch.component.ts
+++ FrontEnd/WinningStrategy/src/app/components/pitch/pitch.component.ts
@@ -7,30 +7,30 @@ import { Component } from '@angular/core';
 })
 export class PitchComponent {
     players = [
-        { name: 'PT', role: 'Role 1', x: 5, y: 50.2 },
-
-        { name: 'TS', role: 'Role 2', x: 22, y: 25 },
-        { name: 'DC', role: 'Role 3', x: 20, y: 40.8 },
-        { name: 'DC', role: 'Role 4', x: 20, y: 59.4 },
-        { name: 'TD', role: 'Role 5', x: 22, y: 75.4 },
-
-        { name: 'CDC', role: 'Role 6', x: 41, y: 50.2 },
-        { name: 'CC', role: 'Role 7', x: 48, y: 33 },
-        { name: 'CC', role: 'Role 8', x: 48, y: 66.6 },
-
-        { name: 'AD', role: 'Role 9', x: 74, y: 80.3 },
-        { name: 'AS', role: 'Role 10', x: 74, y: 19.2 },
-        { name: 'ATT', role: 'Role 11', x: 75, y: 50.2 },
+        { id: 1, name: 'PT', role: 'Role 1', x: 5, y: 50.2 },
+        { id: 2, name: 'TS', role: 'Role 2', x: 22, y: 25 },
+        { id: 3, name: 'DC', role: 'Role 3', x: 20, y: 40.8 },
+        { id: 4, name: 'DC', role: 'Role 4', x: 20, y: 59.4 },
+        { id: 5, name: 'TD', role: 'Role 5', x: 22, y: 75.4 },
+        { id: 6, name: 'CDC', role: 'Role 6', x: 41, y: 50.2 },
+        { id: 7, name: 'CC', role: 'Role 7', x: 48, y: 33 },
+        { id: 8, name: 'CC', role: 'Role 8', x: 48, y: 66.6 },
+        { id: 9, name: 'AD', role: 'Role 9', x: 74, y: 80.3 },
+        { id: 10, name: 'AS', role: 'Role 10', x: 74, y: 19.2 },
+        { id: 11, name: 'ATT', role: 'Role 11', x: 75, y: 50.2 },
     ];
 
     selectedColor = 'blue';
     selectedPlayer = this.players[0];
     tacticalNotes = '';
     defaultModule = '4-3-3';
+    previousPositions: any = null;
+    lastMovedPlayer: any = null;
 
     onDragStart(event: DragEvent, player: any) {
         event.dataTransfer?.setData('text/plain', JSON.stringify(player));
         this.selectedPlayer = player;
+        this.saveCurrentPosition(player); // Save the current position before starting a new drag
     }
 
     onDragOver(event: DragEvent) {
@@ -47,11 +47,12 @@ export class PitchComponent {
             const dropY = (event.offsetY / target.clientHeight) * 100;
 
             const targetPlayer = this.players.find(
-                (p) => p.name === player.name
+                (p) => p.id === player.id
             );
             if (targetPlayer) {
                 targetPlayer.x = dropX;
                 targetPlayer.y = dropY;
+                this.lastMovedPlayer = targetPlayer; // Update the last moved player
             }
         }
     }
@@ -66,74 +67,62 @@ export class PitchComponent {
         switch (module) {
             case '4-3-3':
                 this.players = [
-                    { name: 'PT', role: 'Role 1', x: 5, y: 50.2 },
-
-                    { name: 'TS', role: 'Role 2', x: 22, y: 25 },
-                    { name: 'DC', role: 'Role 3', x: 20, y: 40.8 },
-                    { name: 'DC', role: 'Role 4', x: 20, y: 59.4 },
-                    { name: 'TD', role: 'Role 5', x: 22, y: 75.4 },
-
-                    { name: 'CDC', role: 'Role 6', x: 41, y: 50.2 },
-                    { name: 'CC', role: 'Role 7', x: 48, y: 33 },
-                    { name: 'CC', role: 'Role 8', x: 48, y: 66.6 },
-
-                    { name: 'AD', role: 'Role 9', x: 74, y: 80.3 },
-                    { name: 'AS', role: 'Role 10', x: 74, y: 19.2 },
-                    { name: 'ATT', role: 'Role 11', x: 75, y: 50.2 },
+                    { id: 1, name: 'PT', role: 'Role 1', x: 5, y: 50.2 },
+                    { id: 2, name: 'TS', role: 'Role 2', x: 22, y: 25 },
+                    { id: 3, name: 'DC', role: 'Role 3', x: 20, y: 40.8 },
+                    { id: 4, name: 'DC', role: 'Role 4', x: 20, y: 59.4 },
+                    { id: 5, name: 'TD', role: 'Role 5', x: 22, y: 75.4 },
+                    { id: 6, name: 'CDC', role: 'Role 6', x: 41, y: 50.2 },
+                    { id: 7, name: 'CC', role: 'Role 7', x: 48, y: 33 },
+                    { id: 8, name: 'CC', role: 'Role 8', x: 48, y: 66.6 },
+                    { id: 9, name: 'AD', role: 'Role 9', x: 74, y: 80.3 },
+                    { id: 10, name: 'AS', role: 'Role 10', x: 74, y: 19.2 },
+                    { id: 11, name: 'ATT', role: 'Role 11', x: 75, y: 50.2 },
                 ];
                 break;
             case '4-4-2':
                 this.players = [
-                    { name: 'PT', role: 'Role 1', x: 5, y: 50.2 },
-
-                    { name: 'TS', role: 'Role 2', x: 22, y: 25 },
-                    { name: 'DC', role: 'Role 3', x: 20, y: 40.8 },
-                    { name: 'DC', role: 'Role 4', x: 20, y: 59.4 },
-                    { name: 'TD', role: 'Role 5', x: 22, y: 75.4 },
-
-                    { name: 'ES', role: 'Role 6', x: 50, y: 21 },
-                    { name: 'CC', role: 'Role 7', x: 48, y: 40.8 },
-                    { name: 'CC', role: 'Role 8', x: 48, y: 59.4 },
-                    { name: 'ED', role: 'Role 9', x: 50, y: 79.4 },
-
-                    { name: 'AT', role: 'Role 10', x: 73, y: 47.2 },
-                    { name: 'ATT', role: 'Role 11', x: 82, y: 52 },
+                    { id: 1, name: 'PT', role: 'Role 1', x: 5, y: 50.2 },
+                    { id: 2, name: 'TS', role: 'Role 2', x: 22, y: 25 },
+                    { id: 3, name: 'DC', role: 'Role 3', x: 20, y: 40.8 },
+                    { id: 4, name: 'DC', role: 'Role 4', x: 20, y: 59.4 },
+                    { id: 5, name: 'TD', role: 'Role 5', x: 22, y: 75.4 },
+                    { id: 6, name: 'ES', role: 'Role 6', x: 50, y: 21 },
+                    { id: 7, name: 'CC', role: 'Role 7', x: 48, y: 40.8 },
+                    { id: 8, name: 'CC', role: 'Role 8', x: 48, y: 59.4 },
+                    { id: 9, name: 'ED', role: 'Role 9', x: 50, y: 79.4 },
+                    { id: 10, name: 'AT', role: 'Role 10', x: 73, y: 47.2 },
+                    { id: 11, name: 'ATT', role: 'Role 11', x: 82, y: 52 },
                 ];
                 break;
             case '5-4-1':
                 this.players = [
-                    { name: 'PT', role: 'Role 1', x: 5, y: 50.2 },
-
-                    { name: 'TS', role: 'Role 2', x: 24, y: 20 },
-                    { name: 'DCS', role: 'Role 3', x: 22, y: 35 },
-                    { name: 'DC', role: 'Role 4', x: 22, y: 50.2 },
-                    { name: 'DCD', role: 'Role 5', x: 22, y: 65.5 },
-                    { name: 'TD', role: 'Role 6', x: 24, y: 83.4 },
-
-                    { name: 'ES', role: 'Role 7', x: 44, y: 25 },
-                    { name: 'CC', role: 'Role 8', x: 42, y: 42 },
-                    { name: 'CC', role: 'Role 9', x: 43, y: 56.5 },
-                    { name: 'ED', role: 'Role 10', x: 44, y: 73.4 },
-
-                    { name: 'ATT', role: 'Role 11', x: 68, y: 50 },
+                    { id: 1, name: 'PT', role: 'Role 1', x: 5, y: 50.2 },
+                    { id: 2, name: 'TS', role: 'Role 2', x: 24, y: 20 },
+                    { id: 3, name: 'DCS', role: 'Role 3', x: 22, y: 35 },
+                    { id: 4, name: 'DC', role: 'Role 4', x: 22, y: 50.2 },
+                    { id: 5, name: 'DCD', role: 'Role 5', x: 22, y: 65.5 },
+                    { id: 6, name: 'TD', role: 'Role 6', x: 24, y: 83.4 },
+                    { id: 7, name: 'ES', role: 'Role 7', x: 44, y: 25 },
+                    { id: 8, name: 'CC', role: 'Role 8', x: 42, y: 42 },
+                    { id: 9, name: 'CC', role: 'Role 9', x: 43, y: 56.5 },
+                    { id: 10, name: 'ED', role: 'Role 10', x: 44, y: 73.4 },
+                    { id: 11, name: 'ATT', role: 'Role 11', x: 68, y: 50 },
                 ];
                 break;
             case '3-5-2':
                 this.players = [
-                    { name: 'PT', role: 'Role 1', x: 5, y: 50.2 },
-
-                    { name: 'DCS', role: 'Role 2', x: 22, y: 36 },
-                    { name: 'DC', role: 'Role 3', x: 20, y: 50 },
-                    { name: 'DCD', role: 'Role 5', x: 22, y: 64 },
-
-                    { name: 'ES', role: 'Role 4', x: 51, y: 18 },
-                    { name: 'CC', role: 'Role 6', x: 45, y: 35 },
-                    { name: 'CDC', role: 'Role 7', x: 40, y: 50.2 },
-                    { name: 'CC', role: 'Role 8', x: 49, y: 63 },
-                    { name: 'ED', role: 'Role 9', x: 51, y: 80.3 },
-
-                    { name: 'AT', role: 'Role 10', x: 71, y: 46 },
-                    { name: 'ATT', role: 'Role 11', x: 82, y: 53 },
+                    { id: 1, name: 'PT', role: 'Role 1', x: 5, y: 50.2 },
+                    { id: 2, name: 'DCS', role: 'Role 2', x: 22, y: 36 },
+                    { id: 3, name: 'DC', role: 'Role 3', x: 20, y: 50 },
+                    { id: 4, name: 'DCD', role: 'Role 5', x: 22, y: 64 },
+                    { id: 5, name: 'ES', role: 'Role 4', x: 51, y: 18 },
+                    { id: 6, name: 'CC', role: 'Role 6', x: 45, y: 35 },
+                    { id: 7, name: 'CDC', role: 'Role 7', x: 40, y: 50.2 },
+                    { id: 8, name: 'CC', role: 'Role 8', x: 49, y: 63 },
+                    { id: 9, name: 'ED', role: 'Role 9', x: 51, y: 80.3 },
+                    { id: 10, name: 'AT', role: 'Role 10', x: 71, y: 46 },
+                    { id: 11, name: 'ATT', role: 'Role 11', x: 82, y: 53 },
                 ];
                 break;
             default:
@@ -141,8 +130,22 @@ export class PitchComponent {
         }
     }
 
+    saveCurrentPosition(player: any) {
+        this.previousPositions = { ...player };
+    }
+
     resetPositions() {
         this.setPlayersForModule(this.defaultModule);
+    }
+
+    restorePreviousPosition() {
+        if (this.previousPositions) {
+            const player = this.players.find(p => p.id === this.previousPositions.id);
+            if (player) {
+                player.x = this.previousPositions.x;
+                player.y = this.previousPositions.y;
+            }
+        }
     }
 
     ngAfterViewInit() {
