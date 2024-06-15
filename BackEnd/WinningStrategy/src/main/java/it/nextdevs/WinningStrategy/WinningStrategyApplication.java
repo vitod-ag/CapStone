dd542f84d4bad6d9af1310da2934ac271f402d39
vitod-ag <116908757+vitod-ag@users.noreply.github.com>
2024-06-15 11:58:42 +0200

    commi BackEnd

---
+++ BackEnd/WinningStrategy/src/main/java/it/nextdevs/WinningStrategy/WinningStrategyApplication.java
@@ -0,0 +1,13 @@
+package it.nextdevs.WinningStrategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinningStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinningStrategyApplication.class, args);
+	}
+
+}
